3925fc7eab4e2d93139808bb6c3d4d9f560e738b
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2024-08-01 08:04:03 +0200

    Add movie view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 // app specific components
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
-
+import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
   selector: 'app-root',
@@ -13,28 +13,6 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
 })
 export class AppComponent {
   title = 'mySciFi-Angular-client';
-
-
-  //data for testing display
-  myUrl = 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX1000_.jpg';
-  movies = [
-    {
-      Title: 'The Matrix',
-      Description: 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.',
-      rating: 5,
-      Url: 'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX1000_.jpg'
-    },
-    {
-      Title: 'The Matrix Reloaded',
-      Description: 'Neo and the rebel leaders estimate that they have 72 hours until 250,000 probes discover Zion and destroy it and its inhabitants. During this, Neo must decide how he can save Trinity from a dark fate in his dreams.',
-      rating: 4,
-    },
-    {
-      Title: 'The Matrix Revolutions',
-      Description: 'The human city of Zion defends itself against the massive invasion of the machines as Neo fights to end the war at another front while also opposing the rogue Agent Smith.',
-      rating: 3,
-    }
-  ];
 
   makeUpperCase(value: string): string {
     return value.toUpperCase();
@@ -56,4 +34,11 @@ export class AppComponent {
       width: '280px'
     });
   }
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
+    });
+  }
+  
 }
